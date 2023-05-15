c59e7eb02e1024fd8b41b7fd3dbc86dbbc3f0b61
{alexius <{alexiussavage0@gmail.com}>
2023-05-15 16:45:46 -0700

    added REadme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sav Weather App
+## I created a web application that tells you the weather of any city in the US, it includes the daily weather temperature, humidity, and the wind speeds of said state. This project helped me better understand API's and how to impliment them and how to fetch data from the API's servers. This application can be used to tell the weather of the area and plan your day accordingly.
+
+## Table of contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+## Use on a typical browser such as Google Chrome or Safari.
+## Usage
+## This page can be used to get the weekly weather of your area and plan your day according to the weather. It can also be used as an example of how to fetch API info and how to implement it into the webpage.
+## Credits
+## This webpage was designed and coded by Alexius Savage with some help from slack tutors and open weather API documentation.
+## License
+## Please refer to license on github.
--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let humidity = document.querySelector('.humidity-indicator>.value');
 let wind = document.querySelector('.wind-indicator>.value');
 let image = document.querySelector('weather-image');
 let temperature = document.querySelector('.temp>.value');
-let apiKey = 'b50e4ce47df2796b12bbad5dd2db25bf';
+let weekDayWeather = document.querySelector('.forecast')
+let DayAPI = '0562462fb984b292b6802663f61c2a03'
+let apiKey = 'b50e4ce47df2796b12bbad5dd2db25bf'
+let weeklyWeatherEndpoint = 'api.openweathermap.org/data/2.5/forecast?q={new york}&appid=' + DayAPI;
 let weatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=' + apiKey;
 let getWeatherbyCity = async (city) => {
     let endpoint = weatherEndpoint + '&q=' + city;
@@ -13,6 +16,12 @@ let getWeatherbyCity = async (city) => {
     let weather = await response.json();
     return weather;
 }
+let getWeatherbyDay = async (weekDayWeather) => {
+    let endpoint = weeklyWeatherEndpoint + '&q=' + weekDayWeather;
+    let response = await fetch(endpoint);
+    let weather = await response.json();
+    return weather;
+}   
 
 searchInp.addEventListener('keydown', async (e) => {
     if(e.keyCode === 13) {
