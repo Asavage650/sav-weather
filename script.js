ef4540b19a41beaa6f1bfa5718b95b5076b993f6
{alexius <{alexiussavage0@gmail.com}>
2023-08-21 21:05:25 -0700

    making images folder

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let wind = document.querySelector('.wind-indicator>.value');
 let image = document.querySelector('weather-image');
 let temperature = document.querySelector('.temp>.value');
 let weekDayWeather = document.querySelector('.forecast')
-let DayAPI = '0562462fb984b292b6802663f61c2a03'
+// let DayAPI = '0562462fb984b292b6802663f61c2a03'
 let apiKey = 'b50e4ce47df2796b12bbad5dd2db25bf'
-let weeklyWeatherEndpoint = 'api.openweathermap.org/data/2.5/forecast?q={new york}&appid=' + DayAPI;
+let weeklyWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&appid=' + apiKey;
 let weatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=' + apiKey;
 let getWeatherbyCity = async (city) => {
     let endpoint = weatherEndpoint + '&q=' + city;
@@ -34,6 +34,7 @@ let updateCurrentWeather = (data) => {
     city.textContent = data.name + ',' + data.sys.country;
     day.textContent = dayofWeek();
     humidity.textContent = data.main.humidity
+    console.log(humidity)
     let windDirection 
     let deg = data.wind.deg;
     if (deg > 45 && deg <= 135){
@@ -47,6 +48,9 @@ let updateCurrentWeather = (data) => {
     }
     wind.textContent = windDirection + ',' + data.wind.speed;
     temperature.textContent = data.main.temp > 0 ? '+' + Math.round(data.main.temp) : Math.round(data.main.temp);
+}
+let forthcommingWeather = (data) => {
+    city.textContent = data.name + ',' + data.sys.day
 }
 
 let dayofWeek = () => {
